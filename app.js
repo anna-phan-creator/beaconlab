3dcd6dbacf37a6546e4758663c508bc3a2bc5370
Anna Phan <annahkp96@gmail.com>
2020-06-05 18:11:01 +0200

    sankt jacobs kyrka + framsida

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = {}
 app.beaconRegions =
 [
 	{
-		id: 'page-feet',
+		id: 'page-jacob',
 		//uuid:'dcec7cb2-f20c-4113-abdc-35515633c019',
 		uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D',
 		major: 3560,
